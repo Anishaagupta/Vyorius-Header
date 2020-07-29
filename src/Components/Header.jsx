3eef20365c27328d4ebec50e69bccb6cea88234a
Anishaagupta <anisha8057@gmail.com>
2020-07-29 18:13:05 +0530

    --- Auto-Git Commit ---

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './HeaderStyle.css';
-import Image from './Image';
 import logo from '../assets/c.png';
 import name from '../assets/b.png';
+import pic from '../assets/a.png';
 
 const Header = () => {
   return (
     <div>
       <div className='header'>
-        <Image img={logo} className='logo1' />
-        <Image img={name} className='logo2' />
+        <img src={logo} alt='images' className='logo1' />
+        <div className='sec'>
+          <img src={name} alt='logoimg' className='logo2' />
+        </div>
+      </div>
+      <div className='container'>
+        <div className='main'>
+          <img src={pic} alt='img' className='logo3' />
+          <div className='text'></div>
+        </div>
       </div>
     </div>
   );
--- src/Components/Image.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Logoimg.css';
-
-function Image(props) {
-  return <img src={props.img} alt='images' className='logo1' />;
-}
-export default Image;
